31533fc947ea101ceca245dd03d52019e6acdac4
Sagar Gupta <710sagar@gmail.com>
2022-07-02 16:08:34 -0400

    Place Order and courses modified

--- myapp/views.py
+++ myapp/views.py
@@ -66,3 +66,6 @@ def detail_old(request, top_no):
 def courses(request):
     courlist = Course.objects.all().order_by('id')
     return render(request, 'myapp/courses.html', {'courlist': courlist})
+
+def place_order(request):
+    return render(request, 'myapp/place_order.html')
