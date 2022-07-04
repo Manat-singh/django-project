b1f204f6073163153f282205141252c8c535b9c7
Priyanka Mogha <priyankamogha97@gmail.com>
2022-07-04 12:05:05 -0400

    Optional field

--- myapp/forms.py
+++ myapp/forms.py
@@ -7,4 +7,4 @@ class InterestForm(forms.Form):
                (1, 'Yes')]
     interested = forms.CharField(label='Interested', widget=forms.RadioSelect(choices=CHOICES))
     levels = forms.IntegerField(min_value=1, initial=1)
-    comments = forms.CharField(label='Additional Comments', widget=forms.Textarea)
+    comments = forms.CharField(label='Additional Comments', widget=forms.Textarea, required=False)
