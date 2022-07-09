8cec6d0b3e015944183c6f201733da544b87c419
Ripudaman Singh Uppal <43295539+ripudaman97@users.noreply.github.com>
2022-07-09 16:10:14 -0400

    Lab 8 - forms
    1, 2 (a,b,c,d,f)

--- myapp/forms.py
+++ myapp/forms.py
@@ -8,3 +8,18 @@ class InterestForm(forms.Form):
     interested = forms.CharField(label='Interested', widget=forms.RadioSelect(choices=CHOICES))
     levels = forms.IntegerField(min_value=1, initial=1)
     comments = forms.CharField(label='Additional Comments', widget=forms.Textarea)
+
+CHOICES = [(0, 'No'),
+            (1, 'Yes')]
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('student', 'course', 'levels', 'order_date')
+        widgets = {
+            'student': forms.RadioSelect(choices=CHOICES),
+            'order_date': forms.SelectDateWidget
+        }
+
+
