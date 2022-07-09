4b15bfd35d65728c418fa8f64dde6bd48d19ff37
Manat Jyot Singh <90211273+Manat-singh@users.noreply.github.com>
2022-07-09 16:37:50 -0400

    assignment 8 - question 1

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from myapp.models import Order
+from myapp.models import Order, Student, Course
 
 
 class InterestForm(forms.Form):
@@ -8,3 +8,10 @@ class InterestForm(forms.Form):
     interested = forms.CharField(label='Interested', widget=forms.RadioSelect(choices=CHOICES))
     levels = forms.IntegerField(min_value=1, initial=1)
     comments = forms.CharField(label='Additional Comments', widget=forms.Textarea, required=False)
+
+
+class OrderForm(forms.ModelForm):
+    student = forms.ChoiceField(widget=forms.RadioSelect, queryset=Student.objects.all())
+    course = forms.ChoiceField(widget=forms.RadioSelect, queryset=Course.objects.all())
+    levels = forms.IntegerField(min_value=1, initial=1)
+    order_date = forms.DateField(widget=forms.SelectDateWidget)
