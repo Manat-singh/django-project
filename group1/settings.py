f7929bf53a6b94b0474189a4f72cf21188e04d83
Sagar Gupta <710sagar@gmail.com>
2022-07-28 01:17:44 -0400

    Password encrypt

--- group1/settings.py
+++ group1/settings.py
@@ -33,7 +33,7 @@ MEDIA_URL = '/media/'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'elearningweb.2022@gmail.com'
-EMAIL_HOST_PASSWORD = 'pbeamhbqywytkvdc'
+EMAIL_HOST_PASSWORD = 'pbkdf2_sha256$320000$vGTmecSXPsWI3lGDOOzEhv$7OKHyWo3nbbj2lAKkvCWG9M0B2RXGEIUAl2hDB6Y/II='
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
