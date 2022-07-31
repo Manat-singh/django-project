c7b0b192f8c8d71e7ebf9e543f8d6ffd3f0611b1
Sagar Gupta <710sagar@gmail.com>
2022-07-31 16:25:50 -0400

    Bug fix for showing only one result

--- myapp/admin.py
+++ myapp/admin.py
@@ -17,11 +17,23 @@ class StudentAdmin(admin.ModelAdmin):
 
     def level(self, obj):
         levels = Order.objects.filter(student=obj)
+        count = 0;
+        levelList = ''
         for level in levels:
-            return level.levels;
+            levelList += str(level.levels);
+            count += 1
+            if (count < len(levels)):
+                levelList += ", "
+        return levelList
     def registered_courses(self, obj):
         courses = Order.objects.filter(student=obj)
+        courseList = ""
+        count = 0;
         for course in courses:
-            return course.course.name;
+            courseList += course.course.name
+            count +=1
+            if(count < len(courses)):
+                courseList += ", "
+        return courseList
 
 admin.site.register(Student, StudentAdmin)
